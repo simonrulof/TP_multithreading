b794b2342c65a5e0177bb8f2cd8ffb9aff6bf305
simonrulof <simonber2001@gmail.com>
2023-11-24 10:25:16 +0100

    actualized Boss, Minion classes

--- Boss.py
+++ Boss.py
@@ -1,5 +1,24 @@
-import QueueClient
+from manager import QueueClient
+import time
+from Task import Task
 
 class Boss(QueueClient):
     def __init__(self):
-        super()
+        super().__init__()
+
+    
+    def run(self):
+        i = 1
+        while True:
+            tache = Task(i)
+            self.tasks.put(tache)
+            time.sleep(4)
+            result = self.results.get()
+            print("boss a récupéré resultat = " + str(result[0]) + " pour un temps de " + str(result[1]))
+            
+            i+=1
+
+
+if __name__ == "__main__":
+    b = Boss()
+    b.run()
--- Minion.py
+++ Minion.py
@@ -1,5 +1,19 @@
-import QueueClient
+from manager import QueueClient
+import time
+import Task
 
 class Minion(QueueClient):
-    def __init(self):
-        pass
+    def __init__(self):
+        super().__init__()
+
+    
+    def run(self):
+        while True:
+            time.sleep(1)
+            task = self.tasks.get()
+            result = task.work()
+            self.results.put((task.x, task.time))
+
+if __name__ == "__main__":
+    m = Minion()
+    m.run()
