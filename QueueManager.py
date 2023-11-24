c26bf5d165d831bc8752d1c49adeb04f4a501066
simonrulof <simonber2001@gmail.com>
2023-11-24 09:16:44 +0100

    initialized prof's class task and manager

--- QueueManager.py
+++
@@ -1,6 +0,0 @@
-
-from multiprocessing.managers import BaseManager
-
-class QueueManageur(BaseManager):
-    def __init__(self):
-        pass
--- Task.py
+++ Task.py
@@ -1,11 +1,21 @@
+import time
+
+import numpy as np
+
+
 class Task:
-    def __init__(self, ID, size, a, b, time):
-        self.time = time
-        self.ID = ID
-        self.size = size
-        self.a = a
-        self.b = b
-        self.x = None
+    def __init__(self, identifier):
+        self.identifier = identifier
+        # choosee the size of the problem
+        self.size = np.random.randint(300, 3_000)
+        # Generate the input of the problem
+        self.a = np.random.rand(self.size, self.size)
+        self.b = np.random.rand(self.size)
+        # prepare room for the results
+        self.x = np.zeros((self.size))
+        self.time = 0
 
     def work(self):
-        pass
+        start = time.perf_counter()
+        self.x = np.linalg.solve(self.a, self.b)
+        self.time = time.perf_counter() - start
